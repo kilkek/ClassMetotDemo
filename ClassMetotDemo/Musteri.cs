e1c96f02d8476c471381f578071c2883eddf6e3e
DESKTOP-8LTPLL2\kafkef <ffarukkeklik@gmail.com>
2021-08-17 10:54:32 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int TcKimlikNo { get; set; }
+        public string MusteriAdi { get; set; }
+        public int MusteriYasi { get; set; }
+        public int MusteriNo { get; set; }
+    }
+}
